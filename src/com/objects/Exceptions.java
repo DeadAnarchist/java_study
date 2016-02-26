9c7909d417d8695a90ec786539210bdc588d0dc8
DeadAnarchist <79815595120@yandex.ru>
2016-02-26 22:28:23 +0300

    Добавлен пример создания своих исключений

--- src/com/objects/Exceptions.java
+++ src/com/objects/Exceptions.java
@@ -1,21 +1,39 @@
 package com.objects;
 
+class MyExeption extends Exception {
+    @Override
+    public void printStackTrace() {
+        System.err.println("It's my first Exception");
+    }
+}
+
 public class Exceptions {
     public static void main(String[] args) {
         String[] st = new String[2];
 
         try {
             System.out.println(st[5]);
-        } catch (ArrayIndexOutOfBoundsException ex) {
+        } catch (ArrayIndexOutOfBoundsException ex1) {
             System.out.println("Индекс элемента находился вне границ массива");
         }
 
 
         try {
             System.out.println(st[5]);
-        } catch (Exception ex) {
+        } catch (Exception ex2) {
             System.out.println("Ошибка!");
         }
 
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (Exception ex3) {
+            System.out.println("Принудительно вызванное исключение!");
+        }
+
+        try {
+            throw new MyExeption();
+        } catch (Exception ex4) {
+            ex4.printStackTrace();
+        }
     }
 }
